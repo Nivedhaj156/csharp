d268c06baadb2f97cb55fe5bea0dc3b1c14f9168
Nivedhaj156 <102027948+Nivedhaj156@users.noreply.github.com>
2022-03-22 19:32:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concepts_of_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "dog", "cat", "animal", "1", "2", "3" };
+            for(int i=0;i<name.Length;i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            Console.WriteLine("\n");
+            foreach(string names in name)  //array values are stored in string variable
+            {
+                Console.WriteLine(names);   
+            }
+
+            string[] name1 = { "dog", "cat", "animal", "1", "2", "3" };
+            int j = 0;
+            while(j<name1.Length)
+            {
+                Console.WriteLine(name1[j]);
+                j++;
+            }
+
+        }
+    }
+}
